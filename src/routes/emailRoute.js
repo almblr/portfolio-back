f98a7a94ffd919fe03d6429ce2d3d4001887e71e
Mehdi Birlakhdar <birkyo@gmail.com>
2025-07-15 19:03:44 +0200

    test route

--- src/routes/emailRoute.js
+++ src/routes/emailRoute.js
@@ -4,5 +4,8 @@ import Router from 'express'
 const router = Router()
 
 router.post('/email/send', emailController.sendEmail)
+router.get('/email/test', (req, res) => {
+  res.status(200).json({ success: 'it works' })
+})
 
 export default router
